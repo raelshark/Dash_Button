6421342a6928d9d6e739072715ec68bf1d9b9066
Greg Norman <greg@notthegolfer.com>
2017-06-29 03:43:20 -0400

    Removing all files in .gitignore

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  dash: {
-    MAC_address: "50:f5:da:aa:56:6d"
-  },
-  target: {
-    name: "IFTTT",
-    url: "https://maker.ifttt.com/trigger/dash_pressed/with/key/dg1IbgikKAYnxmYq-A57pa"
-  }
-};
